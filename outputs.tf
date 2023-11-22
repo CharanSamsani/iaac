365f1b58e844ae842050f40b074991796c0aa826
Charan <charansamsani.official@gmail.com>
2023-11-22 21:10:52 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,9 @@
+output "instanceID" {
+  description = " Instance ID: "
+  value = aws_instance.myinstance.tags.Name
+}
 
+output "instanceType" {
+  description = "Type of the Instance : "
+  value = aws_instance.myinstance.instance_type
+}
